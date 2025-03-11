7f18ecca56c8cdc7f0962f71c9c6bd50f293abd3
Vishal Kumar <kk5417866@gmail.com>
2025-03-11 17:18:50 +0530

    final commit

--- src/DB/index.js
+++ src/DB/index.js
@@ -1,14 +1,27 @@
 import mysql from 'mysql2'
 import dotenv from 'dotenv';
-
+import fs from 'fs';
+import path from 'path';
+const sslCertPath = path.resolve('DigiCertGlobalRootCA.crt.pem');
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: process.env.DB_URL,
   port: 3306,
-  user: 'root',
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: 'vishal',
+  // set ssl required and set it location
+  
+  
+  ssl: { ca: fs.readFileSync(sslCertPath) },
+ 
+ 
+ 
+  
+  
+  
+  
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -31,4 +44,3 @@ const pool = mysql.createPool({
 const connectDb = pool.promise();
 
 export default connectDb;
-  
